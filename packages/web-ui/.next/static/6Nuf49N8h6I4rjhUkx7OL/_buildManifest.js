e9f8ec344834031fe476a7eb32937664779a1fad
samjwillis97 <sam@williscloud.org>
2025-04-22 23:59:15 +1000

    fix(chore): IDK

--- packages/web-ui/.next/static/6Nuf49N8h6I4rjhUkx7OL/_buildManifest.js
+++
@@ -1,6 +0,0 @@
-(self.__BUILD_MANIFEST = {
-  __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
-  '/_error': ['static/chunks/pages/_error-2a0a141f9d92ba4d.js'],
-  sortedPages: ['/_app', '/_error'],
-}),
-  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
